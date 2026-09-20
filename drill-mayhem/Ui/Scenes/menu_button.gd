4d63950bf57004a69350ed3cfeefad3c43011af1
SupaSavvy <savionstfleur99@gmail.com>
2026-09-20 13:02:48 -0400

    Revert "Uiyayayayay"
    This reverts commit ea2a77bb3005f81de4b863829ebe95cb45859306.

--- drill-mayhem/Ui/Scenes/menu_button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-func _ready() -> void:
-	
-	pressed.connect(_on_pressed)
-
-func _on_pressed() -> void: 
-	$"../../../../../../../../../ButtonClick".play()
-	get_tree().change_scene_to_file("res://Ui/Scenes/main_menu.tscn")
